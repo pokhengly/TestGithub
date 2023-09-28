72f8e7cb8ed313ea220a21c2e25992e2e00098c0
pokhengly <henglypok21012021@gmail.com>
2023-09-28 18:40:33 +0700

    Initial commit

---
+++ app/src/main/java/com/e2/g16/testgithub/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.e2.g16.testgithub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
